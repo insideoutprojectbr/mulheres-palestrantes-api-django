3f600e21941fe73768714373670fd17098956ced
Raissa Ferreira <rai200890@gmail.com>
2017-03-05 03:30:52 -0300

    Transform speaker model into auth model

--- speaker/models.py
+++ speaker/models.py
@@ -1,27 +1,30 @@
 from django_gravatar.helpers import get_gravatar_url
-
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 from django.utils.timezone import now
 
 
-class Speaker(models.Model):
-    name = models.CharField(max_length=100, db_index=True)
+class Speaker(AbstractUser):
     email = models.EmailField(unique=True, max_length=100, db_index=True)
     location = models.CharField(blank=True, null=True, max_length=100, db_index=True)
-    facebook = models.CharField(blank=True, null=True, max_length=20)
-    twitter = models.CharField(blank=True, null=True, max_length=20)
-    github = models.CharField(blank=True, null=True, max_length=20)
-    linkedin = models.CharField(blank=True, null=True, max_length=20)
-    behance = models.CharField(blank=True, null=True, max_length=20)
+    facebook = models.CharField(blank=True, null=True, max_length=50)
+    twitter = models.CharField(blank=True, null=True, max_length=50)
+    github = models.CharField(blank=True, null=True, max_length=50)
+    linkedin = models.CharField(blank=True, null=True, max_length=50)
+    behance = models.CharField(blank=True, null=True, max_length=50)
+    medium = models.CharField(blank=True, null=True, max_length=50)
     image_url = models.URLField(blank=True, null=True, max_length=300)
     site = models.URLField(blank=True, null=True, max_length=300)
     published = models.BooleanField(default=False, db_index=True)
     interests = models.ManyToManyField('Interest')
-    created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return "{}, {}".format(self.name, self.email)
+
+    @property
+    def name(self):
+        return "{} {}".format(self.first_name, self.last_name)
 
     @property
     def photo(self):
@@ -46,6 +49,10 @@ class Speaker(models.Model):
     @property
     def behance_url(self):
         return "https://www.behance.net/{}".format(self.behance) if self.behance else None
+
+    @property
+    def medium_url(self):
+        return "https://medium.com/{}".format(self.medium) if self.medium else None
 
 
 class Interest(models.Model):
